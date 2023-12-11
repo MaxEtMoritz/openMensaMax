8982e013e48a1fee767f59b2e9aabf42a4c8fc6f
MaxEtMoritz <markus@memotech.franken.de>
2023-12-11 23:45:15 +0100

    fix the fix
    forgot to convert to string

--- openmensa_feed_builder.js
+++ openmensa_feed_builder.js
@@ -48,7 +48,7 @@ function parse_days(data) {
     for (const day of data) {
         const xml = {
             $: {
-                date: day.date instanceof Date ? `${day.date.getFullYear().padStart(4,'0')}-${(day.date.getMonth() + 1).padStart(2,'0')}-${day.date.getDate().padStart(2,'0')}` : day.date,
+                date: day.date instanceof Date ? `${day.date.getFullYear().toString().padStart(4,'0')}-${(day.date.getMonth() + 1).toString().padStart(2,'0')}-${day.date.getDate().toString().padStart(2,'0')}` : day.date,
             },
         };
         if (day.closed) {
